73113d41a6d2f00e6e9e53a87bf943565ad5e2f5
mersudinh <56997983+mersudinh@users.noreply.github.com>
2025-12-17 20:42:24 +0100

    Add multiple RSS feed URLs to config

--- app/config.py
+++ app/config.py
@@ -4,7 +4,22 @@
 # Add as many as you want
 RSS_FEEDS = [
     "https://www.nsf.gov/rss/rss_www_news.xml",
-    "https://higheredstrategy.com/feed/"
+    "https://higheredstrategy.com/feed/",
+    "https://www.higheredtoday.org/feed/",
+    "https://www.highereddive.com/feeds/news/",
+    "https://fsapartners.ed.gov/rss/whats-new",
+    "https://agb.org/feed/",
+    "https://www.msche.org/feed/",
+    "https://www.hlcommission.org/feed/",
+    "https://www.abhe.org/feed/",
+    "https://www.wscuc.org/feed/",
+    "https://www.aacc.nche.edu/feed/",
+    "https://www.deac.org/feed/",
+    "https://www.accsc.org/feed/",
+    "https://nwccu.org/feed/",
+    "https://www.higheredcompliance.org/feed/",
+    "https://nc-sara.org/feed/",
+    "https://upcea.edu/feed/"
 ]
 
 # --- Where to POST new articles ---
